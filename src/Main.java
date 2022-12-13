c17be17ac682d34076d8a3676286f2fef5661b11
YuryShageev <schageev.yur@yandex.ru>
2022-12-13 14:22:06 +0300

    Задачи на умножение для учителя

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 public class Main {
 
     public static void main(String[] args) {
 
         System.out.println("Hi");
+        Random random = new Random();
+
+        Set<MultiplicationVariables> multiplicationVariables = new HashSet<>();
+        while (multiplicationVariables.size() < 15) {
+            MultiplicationVariables multiplicationVariables1 = new MultiplicationVariables(random.nextInt(10) +2, random.nextInt(10) + 2);
+            multiplicationVariables.add(multiplicationVariables1);
+            System.out.println(multiplicationVariables1);
+        }
     }
 
 
--- src/MultiplicationVariables.java
+++ src/MultiplicationVariables.java
@@ -1,4 +1,39 @@
+import java.util.Objects;
+import java.util.stream.IntStream;
+
 public class MultiplicationVariables {
 
+    private int a;
+    private int b;
 
+    public MultiplicationVariables(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MultiplicationVariables that = (MultiplicationVariables) o;
+        return a == that.a && b == that.b || a == that.b && b == that.a;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(a) + Objects.hashCode(b);
+    }
+
+    @Override
+    public String toString() {
+        return a + " * " + b + " = " + (a*b);
+    }
 }
